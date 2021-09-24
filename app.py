f2ace4bea55d84c7d21ffb7ca35fda0f0a829580
nediazb10 <nediaz@uninorte.edu.co>
2021-09-23 19:49:53 -0600

    Proyecto terminado!!

--- app.py
+++ app.py
@@ -1,2 +1,12 @@
-numero = input("Digite el numero a verificar: ")
-print("El numero digitado es: ",numero)
+numero = int(input("Digite el numero a verificar: "))
+#print("El numero digitado es: ",numero)
+
+cont = 0
+for i in range(1,numero+1):
+    if numero % i == 0:
+        cont = cont + 1
+
+if cont == 2:
+    print("El numero es primo")
+else:
+    print("El numero no es primo")
